3cf7c497ed5a3d6cf802df73b765201e6c297dfa
abduqulovabdulla3108@gmail.com <abduqulovabdulla3108@gmail.com>
2025-03-06 11:08:08 +0500

    fixed pagination

--- comments/views.py
+++ comments/views.py
@@ -1,28 +1,36 @@
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from posts.models import Post
-from .models import Comment
 from .pagination import CommentListPagination
 from .serializers import CommentSerializer
 
 
-class CommentListCreateView(APIView, CommentListPagination):
+class CommentListCreateView(APIView):
+    pagination_class = CommentListPagination
+
     def get_object(self, slug):
         try:
             post = Post.objects.get(slug=slug)
-        except Post.DoesNotExist as ex:
-            return Response({
-                'error': 'Post Not Found'
-            }, status=404)
+        except Post.DoesNotExist:
+            return Response({'error': 'Post Not Found'}, status=404)
         return post
 
     def get(self, request, slug):
         post = self.get_object(slug)
-        serializer = CommentSerializer(post.comments, many=True)
-        return Response(serializer.data)
+        if isinstance(post, Response):  # Agar post topilmasa, Response qaytariladi
+            return post
+
+        comments = post.comments.all()
+        paginator = self.pagination_class()
+        result_page = paginator.paginate_queryset(comments, request)
+        serializer = CommentSerializer(result_page, many=True)
+        return paginator.get_paginated_response(serializer.data)
 
     def post(self, request, slug):
         post = self.get_object(slug)
+        if isinstance(post, Response):  # Agar post topilmasa, Response qaytariladi
+            return post
+
         data = request.data.copy()
         data['post'] = post.id
 
@@ -31,6 +39,3 @@ class CommentListCreateView(APIView, CommentListPagination):
             serializer.save()
             return Response(serializer.data, status=201)
         return Response(serializer.errors, status=400)
-
-
-
